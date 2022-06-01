b6a5f0d755dec9011c28bb29e10ddd6c960202ce
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-06-01 14:41:18 +0530

    Create MinimizeMaxDifference.java

---
+++ MinimizeMaxDifference.java
@@ -0,0 +1,45 @@
+package ArrayConcept;
+
+import java.util.Arrays;
+
+
+//O(nlogn),O(1)
+
+public class MinimizeMaxDifference {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 3, 9, 12, 16, 20 };
+		int k = 3;
+		int n = arr.length;
+
+		int d = getMinimizedDifference(arr, n, k);
+		System.out.println(d);
+
+	}
+
+	private static int getMinimizedDifference(int[] arr, int n, int k) {
+
+		Arrays.sort(arr);
+
+		int res = arr[n - 1] - arr[0];
+		int mx, mn;
+		int prmax, prmin;
+		prmax = arr[n - 1] - k;
+		prmin = arr[0] + k;
+
+		for (int i = 1; i < n; i++) {
+
+			if (arr[i] >= k) {
+
+				mn = Math.min(arr[i] - k, prmin);
+				mx = Math.max(arr[i - 1] + k, prmax);
+				res = Math.min(res, mx - mn);
+			}
+
+		}
+		return res;
+
+	}
+
+}
